806b16f48de58edd0df2f68d0c2593886b1a02df
BrianHdz <popcorn46290@sbcglobal.net>
2019-12-07 09:59:07 -0800

    Now I'm actually done

--- pGScript.js
+++ pGScript.js
@@ -1,24 +1,124 @@
-// This will create our password
+// Variables 
+var upperCase = (
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+);
+
+var lowerCase = (
+    "abcdefghijklmnopqrstuvwxyz"
+);
+
+var numbers = (
+    "1234567890"
+);
+
+var specialChars = (
+    "!@#$%^&*-=+"
+);
+
+var password = "";
+var blank = "";
+var newPassword = "";
+
+// Trying something new 
+// console.log("The slider value is " + slider.value);
+document.getElementById("length").innerHTML = "Length: 68";
+
+// Slider live data
+document.getElementById("slider").oninput = function () {
+
+    if (document.getElementById("slider").value > 0) {
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else {
+        document.getElementById("length").innerHTML = "Length: 8";
+    }
+}
+
+
+
+// The Prompts to gather booleans, create the password and push it to the text area.
 function gener8Pswd() {
 
+    var addUpperCase = confirm("To add Upper Cased characters in your password, click OK!");
+    if (addUpperCase == true) {
+        var password = upperCase.concat(password);
+    }
+    else {
+        var password = password;
+    }
+    console.log(password);
+
+    var addLowerCase = confirm("To add Lower Cased characters in your password, click OK!");
+    if (addLowerCase == true) {
+        var password = lowerCase.concat(password);
+    }
+    else {
+        var password = password;
+    }
+    console.log(password);
+
+    var addNumbers = confirm("To add Numeric characters in your password, click OK!");
+    if (addNumbers == true) {
+        var password = numbers.concat(password);
+    }
+    else {
+        var password = password;
+    }
+    console.log(password);
+
+    var addSpecialChars = confirm("To add Special characters in your password, click OK!");
+    if (addSpecialChars == true) {
+        var password = specialChars.concat(password);
+    }
+    else {
+        var password = password;
+    }
+    console.log(password);
+
+
+
+    // Check for at least one boolean
+    if (addUpperCase === false &&
+        addLowerCase === false &&
+        addNumbers === false &&
+        addSpecialChars === false) {
+        alert("Choose one type of character or more.");
+
+    }
+    console.log("They chose " + addUpperCase + addLowerCase + addNumbers
+        + addSpecialChars);
+
+
+
     //sets the password length
-    let complexity = document.getElementById("slider").value;
+    var complexity = document.getElementById("slider").value;
     console.log("The new slider/complexity value is " + complexity);
 
-    //password strings
-    let values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*_=-+0123456789";
-
-    let password = "";
-
-    //For Loop
-    for(var i = 0; i <= complexity; i++){
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    // My Old For Loop
+    for (var i = 0; i <= complexity; i++) {
+        newPassword = newPassword + password.charAt(Math.floor(Math.random() * Math.floor(password.length - 1)));
     }
+    
+    console.log("Your full password is " + newPassword);
 
-    // Send to text area
-    document.getElementById("yourPw").value = password;
-    console.log("Your password is " + password);
+
+    // Send password to text area
+    document.getElementById("yourPw").value = newPassword;
+    console.log("Your new password is " + newPassword);
+
+    return newPassword;
 }
+
+
+
+// Send to text area
+function pushPassword() {
+    let myPassword = gener8Pswd();
+    let yourPassword = document.getElementById("yourPw");
+
+    yourPassword.value = myPassword;
+}
+
 
 
 // This will get the text area box content to copy to clipboard.
@@ -29,21 +129,3 @@ function copyPswd() {
     document.execCommand("copy");
     alert("Copied the text: " + copyText.value);
 }
-
-
-
-// Trying something new 
-console.log("The slider value is " + slider.value);
-document.getElementById("length").innerHTML = "Length: 68";
-
-// Slider live data
-document.getElementById("slider").oninput = function (){
-    
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
-    }
-    else{
-        document.getElementById("length").innerHTML = "Length: 8";
-    }
-}
-
